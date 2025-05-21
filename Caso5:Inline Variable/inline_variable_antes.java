65c71dd59dd8c62a6c5742b7d9e948fd2c4d97d1
Alejandro <2889657@alu.murciaeduca.es>
2025-05-21 13:55:28 +0200

    HE refactorizado el caso 5

---
+++ Caso5:Inline Variable/inline_variable_antes.java
@@ -0,0 +1,10 @@
+public class Suma {
+    public static void main(String[] args) {
+        int resultado = sumar(4, 5);
+        System.out.println("La suma es: " + resultado);
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Caso5:Inline Variable/inline_variable_despues.java
@@ -0,0 +1,9 @@
+public class Suma {
+    public static void main(String[] args) {
+        System.out.println("La suma es: " + sumar(4, 5));
+    }
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+}
