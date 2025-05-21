defbb6bda37e3975044f1a2bf68d708ebb6dd029
Alejandro <2889657@alu.murciaeduca.es>
2025-05-21 13:51:07 +0200

    He refactorizado el caso 2

---
+++ Caso2:Rename Variable/rename_variable_antes.java
@@ -0,0 +1,8 @@
+public class Compra {
+    public static void main(String[] args) {
+        double x = 25.5;
+        double y = 5.0;
+        double z = x + y;
+        System.out.println("Total: " + z);
+    }
+}
---
+++ Caso2:Rename Variable/rename_variable_despues.java
@@ -0,0 +1,8 @@
+public class Compra {
+    public static void main(String[] args) {
+        double precioProducto = 25.5;
+        double precioEnvio = 5.0;
+        double precioTotal = precioProducto + precioEnvio;
+        System.out.println("Total: " + precioTotal);
+    }
+}
