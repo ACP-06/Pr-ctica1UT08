e9c92a2de7c05447421f10290567e2acbc89ecf0
Alejandro <2889657@alu.murciaeduca.es>
2025-05-21 13:46:39 +0200

    He refactorizado el caso 1 en ete commit

---
+++ Caso1:Extract Method/extract_method_antes.java
@@ -0,0 +1,6 @@
+public class Mensaje {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, usuario!");
+        System.out.println("Gracias por usar nuestra aplicación.");
+    }
+}
---
+++ Caso1:Extract Method/extract_method_despues.java
@@ -0,0 +1,10 @@
+public class Mensaje {
+    public static void main(String[] args) {
+        mostrarSaludo();
+    }
+
+    public static void mostrarSaludo() {
+        System.out.println("¡Hola, usuario!");
+        System.out.println("Gracias por usar nuestra aplicación.");
+    }
+}
